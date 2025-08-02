d0b9a4560a1426995a1f64f84c540ff2de7b1957
Hemalathavenkamsetty <182388926+Hemalathavenkamsetty@users.noreply.github.com>
2025-08-02 17:42:13 +0530

    Create data.ts

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+import {Doctor} from './types';
+export const doctors: Doctor[] = [
+    {
+        id: '1',
+        name:'Dr.John Smith',
+        specialization:'Cardiologist',
+        profileImage:'https://example.com/join.jpg',
+        availability:'available',
+    },
+    {
+        id: '2',
+        name:'Dr.Alice Johnson',
+        specialization:'Dermatologist',
+        profileImage: 'https://example.com/alice.jpg',
+        availability:'busy',
+    },
+];
